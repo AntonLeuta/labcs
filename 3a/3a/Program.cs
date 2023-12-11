b2703b50d276a19059805b76635b2fa90173bf14
AntonLeuta <118078784+AntonLeuta@users.noreply.github.com>
2023-12-11 13:00:41 +0300

    Add files via upload

---
+++ 3a/3a/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool readFromConsole = true;
+        string input = "";
+
+        // Проверяем флаги командной строки
+        if (args.Length > 0)
+        {
+            if (args[0] == "-f")
+            {
+                readFromConsole = false;
+                if (args.Length < 2)
+                {
+                    Console.WriteLine("Не указан путь к файлу");
+                    return;
+                }
+                try
+                {
+                    input = System.IO.File.ReadAllText(args[1]);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Ошибка при чтении файла: " + ex.Message);
+                    return;
+                }
+            }
+        }
+
+        if (readFromConsole)
+        {
+            Console.WriteLine("Введите последовательность символов, разделенных пробелами:");
+            input = Console.ReadLine();
+        }
+
+        string[] symbols = input.Split(' ');
+
+        int sum = 0;
+        int count = 0;
+
+        foreach (string symbol in symbols)
+        {
+            int code;
+            if (Int32.TryParse(symbol, out code))
+            {
+                sum += code;
+                count++;
+            }
+        }
+
+        if (count > 0)
+        {
+            double average = (double)sum / count;
+            Console.WriteLine("Среднее арифметическое: " + average);
+        }
+        else
+        {
+            Console.WriteLine("Не найдено цифр для вычисления среднего арифметического");
+        }
+    }
+}
---
+++ no.3b/no.3b/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool readFromConsole = true;
+        string input = "";
+
+        // Проверяем флаги командной строки
+        if (args.Length > 0)
+        {
+            if (args[0] == "-f")
+            {
+                readFromConsole = false;
+                if (args.Length < 2)
+                {
+                    Console.WriteLine("Не указан путь к файлу");
+                    return;
+                }
+                try
+                {
+                    input = System.IO.File.ReadAllText(args[1]);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Ошибка при чтении файла: " + ex.Message);
+                    return;
+                }
+            }
+        }
+
+        if (readFromConsole)
+        {
+            Console.WriteLine("Введите числа, разделенные пробелами:");
+            input = Console.ReadLine();
+        }
+
+        string[] numbers = input.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+        double product = 1;
+        double sumOfInverses = 0;
+        int count = 0;
+
+        foreach (string number in numbers)
+        {
+            double value;
+            if (Double.TryParse(number, out value))
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("Ошибка: недопустимое значение числа");
+                    return;
+                }
+                product *= value;
+                sumOfInverses += 1 / value;
+                count++;
+            }
+        }
+
+        if (count > 0)
+        {
+            double geometricMean = Math.Pow(product, 1.0 / count);
+            double harmonicMean = count / sumOfInverses;
+
+            Console.WriteLine("Среднее геометрическое: " + geometricMean);
+            Console.WriteLine("Среднее гармоническое: " + harmonicMean);
+        }
+        else
+        {
+            Console.WriteLine("Не найдено чисел для вычисления среднего геометрического и среднего гармонического");
+        }
+    }
+}
